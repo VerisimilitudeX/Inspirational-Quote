4729370a958e5a780a0bdd6d4f1069f74014aac1
Verisimilitude <acharyapiyush1@outlook.com>
2022-01-21 16:59:28 -0800

    Add files via upload

---
+++ Inspirational_Quote.py
@@ -0,0 +1,59 @@
+"""
+LESSON: 1.2 - Values
+EXERCISE: Inspirational Quote
+"""
+
+#### ---- INPUT ---- ####
+
+# Get INPUT asking for a quote, and ASSIGN to a
+# variable called quote
+quote = input("What's your favorite inspirational quote?")
+
+# Get INPUT asking who said the quote, and ASSIGN to
+# a variable called author
+# ---> TEST AFTER THIS LINE <--- #
+author = input("Who said this? ")
+
+
+#### ---- BORDERS ---- ####
+
+# Create variable called line1 and ASSIGN it a border:
+# "-----~~~~~~~~~***********~~~~~~~~~-----"
+line1 = ".--._,-'_`-._,-'_`-._,-'_`-._,-'_`-,_,-'_`-,_,-'_`-,_,-'_`-,_,-'_`-,."
+
+# Create variable called line2 and ASSIGN it a border:
+# "|| ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~ ||"
+# ---> TEST AFTER THIS LINE <--- #
+line2 =  "/\ \/ ,-' `-._,-' `-._,-' `-._,-' `-._,-' `-._,-' `-._,-' `-._`./ \ \ ."
+
+
+
+#### ---- OUTPUT ---- ####
+
+# PRINT a blank line for readability
+print("")
+
+# PRINT line1
+print(line1)
+
+# PRINT LINE2
+# ---> TEST AFTER THIS LINE <--- #
+print(line2)
+# PRINT "    ", CONCATENATED with the quote
+# ---> TEST AFTER THIS LINE <--- #
+print("    " + quote)
+
+# PRINT "        - ", CONCATENATED with the author
+# ---> TEST AFTER THIS LINE <--- #
+print("        - " + author)
+
+# PRINT line2
+print(line2)
+
+# PRINT line1
+# ---> TEST AFTER THIS LINE <--- #
+print(line1)
+
+
+# Turn in your Coding Exercise.
+
