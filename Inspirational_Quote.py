1392378303bc9d5aa48521a3cb475ab5954d9eea
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-07 17:44:00 -0800

    Update Inspirational_Quote.py

--- Inspirational_Quote.py
+++ Inspirational_Quote.py
@@ -1,8 +1,3 @@
-"""
-LESSON: 1.2 - Values
-EXERCISE: Inspirational Quote
-"""
-
 #### ---- INPUT ---- ####
 
 # Get INPUT asking for a quote, and ASSIGN to a
@@ -11,7 +6,6 @@ quote = input("What's your favorite inspirational quote?")
 
 # Get INPUT asking who said the quote, and ASSIGN to
 # a variable called author
-# ---> TEST AFTER THIS LINE <--- #
 author = input("Who said this? ")
 
 
@@ -23,7 +17,6 @@ line1 = ".--._,-'_`-._,-'_`-._,-'_`-._,-'_`-,_,-'_`-,_,-'_`-,_,-'_`-,_,-'_`-,."
 
 # Create variable called line2 and ASSIGN it a border:
 # "|| ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~ ||"
-# ---> TEST AFTER THIS LINE <--- #
 line2 =  "/\ \/ ,-' `-._,-' `-._,-' `-._,-' `-._,-' `-._,-' `-._,-' `-._`./ \ \ ."
 
 
@@ -37,23 +30,15 @@ print("")
 print(line1)
 
 # PRINT LINE2
-# ---> TEST AFTER THIS LINE <--- #
 print(line2)
 # PRINT "    ", CONCATENATED with the quote
-# ---> TEST AFTER THIS LINE <--- #
 print("    " + quote)
 
 # PRINT "        - ", CONCATENATED with the author
-# ---> TEST AFTER THIS LINE <--- #
 print("        - " + author)
 
 # PRINT line2
 print(line2)
 
 # PRINT line1
-# ---> TEST AFTER THIS LINE <--- #
 print(line1)
-
-
-# Turn in your Coding Exercise.
-
