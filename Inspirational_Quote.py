49f28492d03c3f9c3ca9d0e50a02b7f02a52ee62
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-17 12:57:36 -0800

    Update Inspirational_Quote.py

--- Inspirational_Quote.py
+++ Inspirational_Quote.py
@@ -1,44 +1,22 @@
-#### ---- INPUT ---- ####
-
-# Get INPUT asking for a quote, and ASSIGN to a
-# variable called quote
 quote = input("What's your favorite inspirational quote?")
 
-# Get INPUT asking who said the quote, and ASSIGN to
-# a variable called author
 author = input("Who said this? ")
 
-
-#### ---- BORDERS ---- ####
-
-# Create variable called line1 and ASSIGN it a border:
-# "-----~~~~~~~~~***********~~~~~~~~~-----"
 line1 = ".--._,-'_`-._,-'_`-._,-'_`-._,-'_`-,_,-'_`-,_,-'_`-,_,-'_`-,_,-'_`-,."
 
-# Create variable called line2 and ASSIGN it a border:
-# "|| ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~ ||"
 line2 =  "/\ \/ ,-' `-._,-' `-._,-' `-._,-' `-._,-' `-._,-' `-._,-' `-._`./ \ \ ."
 
-
-
-#### ---- OUTPUT ---- ####
-
-# PRINT a blank line for readability
 print("")
 
-# PRINT line1
 print(line1)
 
-# PRINT LINE2
 print(line2)
-# PRINT "    ", CONCATENATED with the quote
+
 print("    " + quote)
 
-# PRINT "        - ", CONCATENATED with the author
+
 print("        - " + author)
 
-# PRINT line2
 print(line2)
 
-# PRINT line1
 print(line1)
