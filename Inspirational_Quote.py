2a92fb935264906e473541ebad870b5ae646abf9
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-19 17:41:25 -0800

    Update Inspirational_Quote.py

--- Inspirational_Quote.py
+++ Inspirational_Quote.py
@@ -1,22 +1,15 @@
 quote = input("What's your favorite inspirational quote?")
-
 author = input("Who said this? ")
 
 line1 = ".--._,-'_`-._,-'_`-._,-'_`-._,-'_`-,_,-'_`-,_,-'_`-,_,-'_`-,_,-'_`-,."
-
 line2 =  "/\ \/ ,-' `-._,-' `-._,-' `-._,-' `-._,-' `-._,-' `-._,-' `-._`./ \ \ ."
 
 print("")
-
 print(line1)
-
 print(line2)
 
 print("    " + quote)
-
-
 print("        - " + author)
 
 print(line2)
-
 print(line1)
